6d6c9b632c4dc510ce159734169166cf2e367aa3
Rahul T <rahulmanoj@cet.ac.in>
2021-06-10 19:44:10 +0530

    data to localhost

--- src/components/Campaigns/Campaigns.js
+++ src/components/Campaigns/Campaigns.js
@@ -1,142 +1,203 @@
 import { useEffect, useState } from "react";
-import './Campaigns.css'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faArrowAltCircleLeft, faArrowAltCircleRight, faClock, faHeart } from '@fortawesome/free-solid-svg-icons'
+import "./Campaigns.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faArrowAltCircleLeft,
+  faArrowAltCircleRight,
+  faClock,
+  faHeart,
+} from "@fortawesome/free-solid-svg-icons";
 import getRequest from "../../API/getRequest";
 import { createImgURL, toBase64 } from "../../API/utilities";
 import { Link } from "react-router-dom";
 import {
-    FacebookShareButton,
-    TwitterShareButton,
-    WhatsappShareButton,
+  FacebookShareButton,
+  TwitterShareButton,
+  WhatsappShareButton,
 } from "react-share";
-import { faFacebook, faTwitter, faWhatsapp } from "@fortawesome/free-brands-svg-icons";
+import {
+  faFacebook,
+  faTwitter,
+  faWhatsapp,
+} from "@fortawesome/free-brands-svg-icons";
 
-const Campaigns = ({ids}) => {
-    const [campaignData, setCampaignData] = useState();
-    const [campaign, setcampaign] = useState([])
-    useEffect(() => {
-        getRequest('http://localhost:5000/api/get-all-campaigns')
-        .then(data => {
-            console.log(data)
-            setCampaignData(data.data)
+const Campaigns = ({ ids }) => {
+  const [campaignData, setCampaignData] = useState();
+  const [campaign, setcampaign] = useState([]);
+  useEffect(() => {
+    if (!ids) {
+      getRequest("http://localhost:5000/api/get-all-campaigns")
+        .then((data) => {
+          console.log(data);
+          setCampaignData(data.data);
+          localStorage.setItem("campaigns", JSON.stringify(data.data));
         })
-        .catch(err => console.log(err))
-    }, [])
-    useEffect(() => {
-        if(ids && campaignData){
-            console.log(ids);
-            console.log("============================");
-            setcampaign([])
-            ids.forEach(i => {
-                let newP = campaignData.find(p => p._id === i)
-                console.log(newP);
-                setcampaign(state => [...state, newP])
-
-            })
-        } else{
-            console.log('||||||||||||||||||||||');
-            console.log(campaignData);
-            let p = campaignData
-            setcampaign(p)
-            console.log(campaign);
-        }
-    }, [campaignData])
-
-    const createLink = (id) => {
-        return `/campaign-details/${id}`
+        .catch((err) => console.log(err));
     }
-
-    const arrowStyle = {
-        color: '#1a73e8'
+  }, []);
+  useEffect(() => {
+    if (ids) {
+      if (localStorage.getItem("campaigns")) {
+        console.log(ids);
+        console.log("============================");
+        setcampaign([]);
+        ids.forEach((i) => {
+          let newP = JSON.parse(localStorage.getItem("campaigns")).find(
+            (p) => p._id === i
+          );
+          console.log(newP);
+          setcampaign((state) => [...state, newP]);
+        });
+      } else {
+        getRequest("http://localhost:5000/api/get-all-campaigns")
+          .then((data) => {
+            console.log(data);
+            setCampaignData(data.data);
+            localStorage.setItem("campaigns", JSON.stringify(data.data));
+          })
+          .catch((err) => console.log(err));
+      }
+    } else {
+      console.log("||||||||||||||||||||||");
+      console.log(campaignData);
+      let p = campaignData;
+      setcampaign(p);
+      console.log(campaign);
     }
-    const handleLeftScroll = (e) => {
-        document.querySelector('.campaign-container').scrollBy({
-            top: 0,
-            left: -700,
-            behavior: 'smooth'
-        })
-    }
+  }, [campaignData]);
 
-    const handleRightScroll = (e) => {
-        document.querySelector('.campaign-container').scrollBy({
-            top: 0,
-            left: 700,
-            behavior: 'smooth'
-        })
-    }
+  const createLink = (id) => {
+    return `/campaign-details/${id}`;
+  };
 
-    const handleClick = (id) => {
-        window.location = `/campaign-details/${id}`
-    }
+  const arrowStyle = {
+    color: "#1a73e8",
+  };
+  const handleLeftScroll = (e) => {
+    document.querySelector(".campaign-container").scrollBy({
+      top: 0,
+      left: -700,
+      behavior: "smooth",
+    });
+  };
 
+  const handleRightScroll = (e) => {
+    document.querySelector(".campaign-container").scrollBy({
+      top: 0,
+      left: 700,
+      behavior: "smooth",
+    });
+  };
 
-    if(!campaign){
-        return (
-            <div className="isLoading">
-                <h1>Loading...</h1>
-            </div>
-        )
-    }
-    return ( 
-        <div className="Campaigns">
-            <h1 className="camp-title">Campaign</h1>
+  const handleClick = (id) => {
+    window.location = `/campaign-details/${id}`;
+  };
 
-            <div className="left-scroll">
-                <FontAwesomeIcon style={arrowStyle} icon={faArrowAltCircleLeft} size="3x"  onClick={handleLeftScroll} />
-            </div>
+  if (!campaign) {
+    return (
+      <div className="isLoading">
+        <h1>Loading...</h1>
+      </div>
+    );
+  }
+  return (
+    <div className="Campaigns">
+      <h1 className="camp-title">Campaign</h1>
 
-            
-                <div className="campaign-container">
-                    { campaign.map((data) => (
-                    
-                        <>
-                        <div className="camp-container" key={data._id}>
-                        <Link to={createLink(data._id)} onClick={() => handleClick(data._id)}>
-                            <div className="camp-img">
-                                <img className="img" src={createImgURL(data)} alt="" />
-                            </div>
-                            <div className="camp-details-container">
-                                <div className="camp-heading">
-                                    <h1 className="title">{data.title}</h1>
-                                    <p>{data.content}</p>
-                                </div>
-                                <div className="organiser-details">
-                                    <img src="https://kettocdn.gumlet.io/media/individual/1358000/1358229/image/5e58e4fc7b807.jpg?w=50&dpr=1.0" alt="" />
-                                    <p>{data.fundRaiser}</p>
-                                </div>
+      <div className="left-scroll">
+        <FontAwesomeIcon
+          style={arrowStyle}
+          icon={faArrowAltCircleLeft}
+          size="3x"
+          onClick={handleLeftScroll}
+        />
+      </div>
 
-                                <div className="fund-details">
-                                    <h2><span>Rs {data.moneyCollected} </span> raised out of Rs {data.totalAmount}</h2>
-                                    <progress value={data.moneyCollected} max={data.totalAmount}></progress>
-                                </div>
-
-                                <div className="extra-details">
-                                    <div className="time-left">
-                                        <p> <FontAwesomeIcon style={{color: 'grey'}} icon={faClock} /> 91 Days Left</p>
-                                    </div>
-
-                                    <div className="supporters">
-                                        <p><FontAwesomeIcon style={{color: 'red'}} icon={faHeart} /> {data.supporters} Supporters</p>
-                                    </div>
-                                </div>
-                            </div>
-                        </Link>
-                        <div className="share">
-                            <WhatsappShareButton url="http://nerchapetti.com"><FontAwesomeIcon className = "shareicon" icon={faWhatsapp} /> </WhatsappShareButton>
-                            <FacebookShareButton url="http://nerchapetti.com"><FontAwesomeIcon className = "shareicon" icon={faFacebook} /> </FacebookShareButton>
-                            <TwitterShareButton url="http://nerchapetti.com"><FontAwesomeIcon className = "shareicon" icon={faTwitter} /> </TwitterShareButton>
-                        </div>
-                        </div>
-                        </>
-                    ))}
+      <div className="campaign-container">
+        {campaign.map((data) => (
+          <>
+            <div className="camp-container" key={data._id}>
+              <Link
+                to={createLink(data._id)}
+                onClick={() => handleClick(data._id)}
+              >
+                <div className="camp-img">
+                  <img className="img" src={createImgURL(data)} alt="" />
                 </div>
+                <div className="camp-details-container">
+                  <div className="camp-heading">
+                    <h1 className="title">{data.title}</h1>
+                    <p>{data.content}</p>
+                  </div>
+                  <div className="organiser-details">
+                    <img
+                      src="https://kettocdn.gumlet.io/media/individual/1358000/1358229/image/5e58e4fc7b807.jpg?w=50&dpr=1.0"
+                      alt=""
+                    />
+                    <p>{data.fundRaiser}</p>
+                  </div>
 
-            <div className="right-scroll">
-                <FontAwesomeIcon style={arrowStyle} icon={faArrowAltCircleRight} size="3x" onClick={handleRightScroll}/>
+                  <div className="fund-details">
+                    <h2>
+                      <span>Rs {data.moneyCollected} </span> raised out of Rs{" "}
+                      {data.totalAmount}
+                    </h2>
+                    <progress
+                      value={data.moneyCollected}
+                      max={data.totalAmount}
+                    ></progress>
+                  </div>
+
+                  <div className="extra-details">
+                    <div className="time-left">
+                      <p>
+                        {" "}
+                        <FontAwesomeIcon
+                          style={{ color: "grey" }}
+                          icon={faClock}
+                        />{" "}
+                        91 Days Left
+                      </p>
+                    </div>
+
+                    <div className="supporters">
+                      <p>
+                        <FontAwesomeIcon
+                          style={{ color: "red" }}
+                          icon={faHeart}
+                        />{" "}
+                        {data.supporters} Supporters
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </Link>
+              <div className="share">
+                <WhatsappShareButton url="http://nerchapetti.com">
+                  <FontAwesomeIcon className="shareicon" icon={faWhatsapp} />{" "}
+                </WhatsappShareButton>
+                <FacebookShareButton url="http://nerchapetti.com">
+                  <FontAwesomeIcon className="shareicon" icon={faFacebook} />{" "}
+                </FacebookShareButton>
+                <TwitterShareButton url="http://nerchapetti.com">
+                  <FontAwesomeIcon className="shareicon" icon={faTwitter} />{" "}
+                </TwitterShareButton>
+              </div>
             </div>
-        </div>
-     );
-}
- 
+          </>
+        ))}
+      </div>
+
+      <div className="right-scroll">
+        <FontAwesomeIcon
+          style={arrowStyle}
+          icon={faArrowAltCircleRight}
+          size="3x"
+          onClick={handleRightScroll}
+        />
+      </div>
+    </div>
+  );
+};
+
 export default Campaigns;
--- src/components/Parishes/Parishes.js
+++ src/components/Parishes/Parishes.js
@@ -1,134 +1,142 @@
 import { useState, useEffect } from "react";
-import './Parishes.css'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faArrowAltCircleLeft, faArrowAltCircleRight, faShareAlt } from "@fortawesome/free-solid-svg-icons";
+import "./Parishes.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faArrowAltCircleLeft,
+  faArrowAltCircleRight,
+  faShareAlt,
+} from "@fortawesome/free-solid-svg-icons";
 import getRequest from "../../API/getRequest";
 import { createImgURL, toBase64 } from "../../API/utilities";
 import { Link } from "react-router-dom";
 import {
-    FacebookShareButton,
-    LinkedinShareButton,
-    TelegramShareButton,
-    TwitterShareButton,
-    WhatsappShareButton,
+  FacebookShareButton,
+  LinkedinShareButton,
+  TelegramShareButton,
+  TwitterShareButton,
+  WhatsappShareButton,
 } from "react-share";
 
-
-
-const Parishes = ({ids}) => {
-    const [Parishesdata, setParishesdata] = useState();
-    const [Parishes, setParishes] = useState([])
-    useEffect(() => {
-        getRequest('http://localhost:5000/get-all-parishes')
-        .then(data => {
-            setParishesdata(data.data)
-            console.log(data)
-            
-        })
-
-       
-        
-    }, [])
-
-
-    useEffect(() => {
-        if(ids && Parishesdata){
-            console.log(ids);
-            console.log("============================");
-            setParishes([])
-            ids.forEach(i => {
-                let newP = Parishesdata.find(p => p._id === i)
-                console.log(newP);
-                setParishes(state => [...state, newP])
-
-            })
-        } else{
-            console.log('||||||||||||||||||||||');
-            console.log(Parishesdata);
-            let p = Parishesdata
-            setParishes(p)
-            console.log(Parishes);
-        }
-    }, [Parishesdata])
-     
-
-    const arrowStyle = {
-        color: '#1a73e8'
+const Parishes = ({ ids }) => {
+  const [Parishesdata, setParishesdata] = useState();
+  const [Parishes, setParishes] = useState([]);
+  useEffect(() => {
+    if (!ids) {
+      getRequest("http://localhost:5000/get-all-parishes").then((data) => {
+        setParishesdata(data.data);
+        console.log(data);
+        localStorage.setItem("parishes", JSON.stringify(data.data));
+      });
     }
-    const handleLeftScroll = (e) => {
-        document.querySelector('.Parishes-containor').scrollBy({
-            top: 0,
-            left: -700,
-            behavior: 'smooth'
-        })
+  }, []);
+
+  useEffect(() => {
+    if (ids) {
+      if (localStorage.getItem("parishes")) {
+        console.log(ids);
+        console.log("============================");
+        setParishes([]);
+
+        ids.forEach((i) => {
+          let newP = JSON.parse(localStorage.getItem("parishes")).find(
+            (p) => p._id === i
+          );
+          console.log(newP);
+          setParishes((state) => [...state, newP]);
+        });
+      } else {
+        getRequest("http://localhost:5000/get-all-parishes").then((data) => {
+          setParishesdata(data.data);
+          console.log(data);
+          localStorage.setItem("parishes", JSON.stringify(data.data));
+        });
+      }
+    } else {
+      console.log("||||||||||||||||||||||");
+      console.log(Parishesdata);
+      let p = Parishesdata;
+      setParishes(p);
+      console.log(Parishes);
     }
+  }, [Parishesdata]);
 
-   
+  const arrowStyle = {
+    color: "#1a73e8",
+  };
+  const handleLeftScroll = (e) => {
+    document.querySelector(".Parishes-containor").scrollBy({
+      top: 0,
+      left: -700,
+      behavior: "smooth",
+    });
+  };
 
-    const handleRightScroll = (e) => {
-        document.querySelector('.Parishes-containor').scrollBy({
-            top: 0,
-            left: 700,
-            behavior: 'smooth'
-        })
-    }
+  const handleRightScroll = (e) => {
+    document.querySelector(".Parishes-containor").scrollBy({
+      top: 0,
+      left: 700,
+      behavior: "smooth",
+    });
+  };
 
-    if(!Parishes){
-        return (
-            <div className="isLoading">
-                <h1>Loading...</h1>
+  if (!Parishes) {
+    return (
+      <div className="isLoading">
+        <h1>Loading...</h1>
+      </div>
+    );
+  }
+
+  const handleClick = (id) => {
+    window.location = `/parish-details/${id}`;
+  };
+
+  return (
+    <div className="parishes">
+      <h1 className="pari-title">Parish</h1>
+      <div className="left-scroll">
+        <FontAwesomeIcon
+          style={arrowStyle}
+          icon={faArrowAltCircleLeft}
+          size="3x"
+          onClick={handleLeftScroll}
+        />
+      </div>
+      <div className="Parishes-containor">
+        {Parishes.map((data) => (
+          <Link
+            to={`/parish-details/${data._id}`}
+            onClick={() => handleClick(data._id)}
+          >
+            <div className="cards" key={data._id}>
+              <div className="images">
+                <img src={createImgURL(data)} alt="" />
+              </div>
+              <div className="catagory">{data.churchcategory}</div>
+              <div className="churchname">{data.churchName}</div>
+              <div className="about">{data.content}</div>
+              <div className="location">
+                <p>location : {data.location}</p>
+              </div>
+              <div className="share">
+                <WhatsappShareButton url="http://nerchapetti.com">
+                  <FontAwesomeIcon className="shareicon" icon={faShareAlt} />{" "}
+                </WhatsappShareButton>
+              </div>
             </div>
-        )
-    }
-
-    const handleClick = (id) => {
-        window.location = `/parish-details/${id}`
-    }
-
-    return ( 
-        <div className="parishes">
-            <h1 className="pari-title">Parish</h1>
-            <div className="left-scroll">
-                <FontAwesomeIcon style={arrowStyle} icon={faArrowAltCircleLeft} size="3x"  onClick={handleLeftScroll} />
-            </div>
-            <div className="Parishes-containor">
-                { Parishes.map((data) => (
-                    <Link to={`/parish-details/${data._id}`} onClick={() => handleClick(data._id)}>
-                        <div className ="cards" key={data._id}>
-                            <div className="images">
-                                <img src={createImgURL(data)} alt="" />
-                            </div>
-                            <div className="catagory">
-                                    {data.churchcategory}
-                            </div>
-                            <div className="churchname">
-                                {data.churchName}
-                            </div>
-                            <div className="about">
-                                {data.content}
-                            </div>
-                            <div className="location">
-                                <p>location : {data.location}</p>
-                            </div>
-                            <div className="share">
-                            
-                                
-                                
-                                <WhatsappShareButton url="http://nerchapetti.com"><FontAwesomeIcon className = "shareicon" icon={faShareAlt} /> </WhatsappShareButton>
-                            
-                            </div>
-                        </div>
-                    </Link>
-                    
-                ))
-                }
-        
-            </div>
-            <div className="right-scroll">
-                <FontAwesomeIcon style={arrowStyle} icon={faArrowAltCircleRight} size="3x" onClick={handleRightScroll}/>
-            </div>
-        </div>
-     );
-}
+          </Link>
+        ))}
+      </div>
+      <div className="right-scroll">
+        <FontAwesomeIcon
+          style={arrowStyle}
+          icon={faArrowAltCircleRight}
+          size="3x"
+          onClick={handleRightScroll}
+        />
+      </div>
+    </div>
+  );
+};
 
 export default Parishes;
