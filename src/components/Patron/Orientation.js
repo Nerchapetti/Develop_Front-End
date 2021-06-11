f24922cd503d7efc33d57f22e7e788fd94f8609d
Rahul T <rahulmanoj@cet.ac.in>
2021-06-11 11:59:00 +0530

    table

--- src/components/Patron/Orientation.js
+++ src/components/Patron/Orientation.js
@@ -1,16 +1,18 @@
 import React from 'react'
+import "./orientation.css"
+
 
 const Orientation = () => {
     return (
         <div className="orientation">
-          <div className="dates">
+          {/* <div className="dates">
               <h3>Date of orientation</h3>
               <ul>
                   <li>31-12-1994 (Sub-Deacon)</li>
                     <li>15-08-1995 (Deacon)</li>
                     <li>23-09-1995 (Priest)</li>
               </ul>
-          </div>
+          </div> */}
           <div className="tabledata">
               <h1 className="tablehead">List of Churches and Institutions Served</h1>
               <table border="1">
