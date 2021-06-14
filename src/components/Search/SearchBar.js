ad650139e857cd0d3a19d6b7b409e70336e653dd
Rahul T <rahulmanoj@cet.ac.in>
2021-06-14 16:37:46 +0530

    search bar

---
+++ src/components/Search/SearchBar.js
@@ -0,0 +1,46 @@
+import React, { useEffect } from "react";
+import { useState } from "react";
+import getRequest from "../../API/getRequest";
+import SearchResult from "./SearchResult";
+
+const SearchBar = () => {
+  const [keyword, setkeyword] = useState("");
+  const [parishes, setparishes] = useState("")
+  const [error, seterror] = useState(false)
+  useEffect(() => {
+
+      getRequest(`${window.URI}/get-all-parishes`).then((data) => {
+        if(data.status === 'ok'){
+          setparishes(data.data);
+          console.log(data);
+          
+        }
+        
+      }).catch(err => {
+        seterror(true)
+        console.log(err);
+      })
+
+  }, []);
+
+  if(!parishes){
+      return(
+          null
+      )
+  }
+  return (
+    <div className="search-bar">
+      <input
+        autoFocus
+        className="search"
+        type="search"
+        value={keyword}
+        onChange={(e) => setkeyword(e.target.value)}
+        placeholder="search.. "
+      />
+      <SearchResult parishes={parishes} keyword={keyword}/>
+    </div>
+  );
+};
+
+export default SearchBar;
---
+++ src/components/Search/SearchResult.js
@@ -0,0 +1,41 @@
+import React, { useEffect, useState } from 'react'
+import ResultCard from './ResultCard'
+import "./search.css"
+
+const SearchResult = ({keyword, parishes}) => {
+
+    const [result, setresult] = useState([])
+
+    useEffect(() => {
+        let searchResult = parishes.filter(parish => {
+            return parish.about.churchName.toLowerCase().includes(keyword.toLowerCase())
+        })
+        setresult(searchResult)
+        console.log(result);
+    }, [keyword])
+
+    if(!keyword){
+        return null
+    }
+
+
+
+
+    if(result.length === 0 || result === null){
+        return (
+            <div className="search-result">
+            <h1>no results found</h1>
+        </div>
+        )
+    }
+
+    return (
+        <div className="search-result">
+            {result.map(r => (
+                <ResultCard result={r} />
+            ))}
+        </div>
+    )
+}
+
+export default SearchResult
