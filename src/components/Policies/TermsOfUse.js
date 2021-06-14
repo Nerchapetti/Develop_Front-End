1aeb2cea7583aa99761451e258f53820a684a6df
Vyshnav <vyshnavvishnu707@gmail.com>
2021-06-14 22:54:32 +0530

    policy check

--- src/components/Policies/TermsOfUse.js
+++ src/components/Policies/TermsOfUse.js
@@ -6,7 +6,14 @@ export const TermsOfUse = () => {
   return (
     <div>
       <Header />
-      <h1>This is TermsOfUse page</h1>
+      <div className="policy">
+        <h2>Disclaimer</h2>
+        <p>We do not provide any financial return in any form whatsoever, including but not limited to financial securities (debt or equity), interest, dividend, profit share, rewards in cash, to individuals who contribute on Nerchapetti.com.</p>
+        <h2>Any contribution on Nerchapetti.com, by an individual, should not be construed as an investment in any form whatsoever.</h2>
+        <p>
+          
+        </p>          
+      </div>
       <Footer />
     </div>
   );
