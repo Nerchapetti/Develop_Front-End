c24f66ede04f38ad277b6bfbc106631f4067db1d
Rahul T <rahulmanoj@cet.ac.in>
2021-07-04 20:24:44 +0530

    popup

---
+++ src/components/Popup/Popup.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./Popup.css"
+
+const Popup = () => {
+    return (
+        <div className="popup-container">
+            this is a popup
+        </div>
+    )
+}
+
+export default Popup
