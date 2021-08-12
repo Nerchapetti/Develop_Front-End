ef7401a22f90fad2ac63f8dd605d4810cd4fd62f
Vaishnav <vrv.mee@gmail.com>
2021-08-12 22:39:52 +0530

    faq 2.0

--- src/components/HowItWorks/HowItWorks.js
+++ src/components/HowItWorks/HowItWorks.js
@@ -1,6 +1,6 @@
 import Footer from "../Footer/Footer";
 import Header from "../NavBar/Header";
-import './howitworks.css'
+import "./howitworks.css";
 
 const HowItWorks = () => {
   return (
@@ -22,7 +22,6 @@ const HowItWorks = () => {
               processors.
             </p>
             <p>
-              {" "}
               These processors include Banks (for Net-banking, IMPS, NEFT, RTGS
               transactions), Visa, Mastercard, AMEX, Rupay, etc (for credit and
               debit card transactions), Paytm, UPI apps, and other payment
@@ -33,6 +32,27 @@ const HowItWorks = () => {
               This is NOT a service charge to Nerchapetti.com or John Zakariah
               Technologies. These fees go directly to the payment processor and
               help us keep nerchapetti.com a safe place to donate.
+            </p>
+          </div>
+          <div>
+            <h2> Why is Nerchapetti.com not charging any fees for itself? </h2>
+            <p>
+              Actually, we do. We have two plans :-<br /> &emsp;  The BASIC plan and the
+              PREMIUM plan. While we do not collect any service fees for the
+              BASIC plan, we do collect a 10% service fee on the total amount
+              raised through this platform, for the PREMIUM plan.
+            </p>
+            <p>
+              For the PREMIUM plan, we estimate the amount that can be raised
+              via this platform, and budget a part of the service fee expected
+              to be collected in the future, to be utilized for social media
+              advertising and for other digital marketing efforts.
+            </p>
+            <p>
+              For the BASIC plan, however, there are no service fees (other than
+              the bank charges collected by the payment gateway provider), and
+              we believe it will encourage more people to support various needs
+              and causes, without absolute peace of mind.
             </p>
           </div>
         </div>
