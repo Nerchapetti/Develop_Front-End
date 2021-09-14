850a59a8957aa69f915597b9f4cf33c32076964f
Rahul T <rahulmanojcoder@gmail.com>
2021-09-14 21:00:29 +0530

    removed i and t

--- src/components/AboutPage/TeamMember.js
+++ src/components/AboutPage/TeamMember.js
@@ -8,31 +8,13 @@ import {
 import "./about.scss";
 import { Link } from "react-router-dom";
 
-const TeamMember = ({ name, about, img, l, i, t }) => {
+const TeamMember = ({ name, about, img, l }) => {
   return (
     <div className="member">
       <img src={img} alt={name} />
       <h3 className="name">{name}</h3>
       <p className="name-detail">{about}</p>
       <div className="detail-icon">
-        {/* <a href={`https://instagram.com/${i}`}>
-          <span>
-            <FontAwesomeIcon
-              style={{color: '#b0487b'}}
-              className="icon"
-              icon={faInstagram}
-            />
-          </span>
-        </a>
-        <a href={`https://twitter.com/${t}`}>
-          <span>
-            <FontAwesomeIcon
-            style={{color: '#4ea6e8'}}
-              className="icon"
-              icon={faTwitter}
-            />
-          </span>
-        </a> */}
         <a href={`https://linkedin.com/in/${l}`}>
           <span>
             <FontAwesomeIcon
